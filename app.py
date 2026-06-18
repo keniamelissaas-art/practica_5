00e72ef0d363e6dfa362739f192a25950660fcca
keniamelissaas-art <keniamelissa.as@gmail.com>
2026-06-17 18:52:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # app.py
 import streamlit as st
 from modulos.venta import mostrar_venta  # Importamos la función mostrar_venta del módulo venta
+from modulos.login import login
 
 # Llamamos a la función mostrar_venta para mostrar el mensaje en la app
 mostrar_venta()
+login()
