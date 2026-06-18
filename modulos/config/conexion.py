7bd7f4747e4cb59e8b6d29b8dd70457ea997a248
keniamelissaas-art <keniamelissa.as@gmail.com>
2026-06-17 19:08:32 -0600

    Update conexion.py

--- modulos/config/conexion.py
+++ modulos/config/conexion.py
@@ -4,7 +4,7 @@ from mysql.connector import Error
 def obtener_conexion():
     try:
         conexion = mysql.connector.connect(
-            host='bjiezmunaora5oi898fu-mysql.services.clever-cloud.com,
+            host='bjiezmunaora5oi898fu-mysql.services.clever-cloud.com',
             user='ueeb4z38vt9xtz3c',
             password='dSyQ74Vhjr57QxhpEi1e',
             database='bjiezmunaora5oi898fu',
