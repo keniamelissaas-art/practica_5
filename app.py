f9a3c93331d47bddb2a4bb6c09452d53e8cac4b9
keniamelissaas-art <keniamelissa.as@gmail.com>
2026-06-20 19:18:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import streamlit as st
 from modulos.venta import mostrar_venta  # Importamos la función mostrar_venta del módulo venta
 from modulos.login import login
 
-# Llamamos a la función mostrar_venta para mostrar el mensaje en la app
+# Comprobamos si la sesión ya está iniciada
+if "sesion_iniciada" in st.session_state and st.session_state["sesion_iniciada"]:
+# Si la sesión está iniciada, mostrar el contenido de ventas
 mostrar_venta()
+else:
+# Si la sesión no está iniciada, mostrar el login
 login()
